237f09a4d65672da57945a716cb438468e8b885e
lorismilloni <lorismsimon@protonmail.com>
2021-11-10 18:46:18 -0300

    exercicio 1 parte1

---
+++ modulo1/bloco4/script_dia_4.js
@@ -0,0 +1,8 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  bemVindo = 'Bem-vinda, ' + info.personagem;
+  console.log(bemVindo);
